299603b71511e9cf82ace103c022d118c7876af3
root <shyam.udupa@gmail.com>
2019-06-26 09:31:52 +0000

    display status messages to the notebook from the server

--- client/hide.js
+++ client/hide.js
@@ -1,3 +1,9 @@
+function getXsrfToken(){
+    var name = '_xsrf';
+    var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+    return r ? r[1] : undefined;
+}
+
 define(['base/js/namespace', 'base/js/utils'], function(Jupyter, utils) {
     function load_ipython_extension() {
 
@@ -7,20 +13,51 @@ define(['base/js/namespace', 'base/js/utils'], function(Jupyter, utils) {
 	    var payload = {};
             payload.notebook = Jupyter.notebook.notebook_path;
 
-	    var success = function(data){
-		if(data.resp == "success"){
-			location.reload();
-                }
-	    };
-	    var error = function(err) { console.log(err); } ;
-	    utils.ajax(Jupyter.notebook.base_url + "/plutoEx", {
+	    var error = err => { 
+                console.log("ERROR: " + err); 
+            } ;
+
+            var xsrf_token = getXsrfToken();
+            var lastResponseLength = false;
+
+            //modified from: https://gist.github.com/sohelrana820/63f029d3aa12936afbc50eb785c496c0
+	    $.ajax(Jupyter.notebook.base_url + "/plutoEx", {
 		type: "POST",
 		cache: false,
-		dataType: "json",
-		success: success,
+		dataType: "text",
+		success: data => {},
 		error: error,
 		data: JSON.stringify(payload),
-		contentType: 'application/json'
+		contentType: 'application/json',
+                headers: {
+                    'X-XSRFToken': xsrf_token
+                },
+                processData: false,
+                xhrFields: {
+                    onprogress: function(e){
+                        var progressResponse;
+                        var response = e.currentTarget.response;
+                        if(lastResponseLength === false){
+                            progressResponse = response;
+                            lastResponseLength = response.length;
+                        } else {
+                            progressResponse = response.substring(lastResponseLength);
+                            lastResponseLength = response.length;
+                        }
+                        //console.log("STATUS: " + progressResponse);
+                        var parsedResponse = JSON.parse(progressResponse);
+
+                        if (parsedResponse.ok) {
+                            Jupyter.notification_area.widget_dict["notebook"].info(parsedResponse.text);
+                            if (parsedResponse.finished){
+                                location.reload();
+                            }
+                        } else {
+                            Jupyter.notification_area.widget_dict["notebook"].danger("Error!");
+                            console.log(parsedResponse.text);
+                        }
+                    }
+                }
 	    });
         };
 
--- server/remote/RequestExecutor.py
+++ server/remote/RequestExecutor.py
@@ -16,52 +16,60 @@ import urllib.request
 from bson.objectid import ObjectId
 from bson.binary import Binary
 from time import sleep
-from ipykernel.comm import Comm
 
 class RequestHandler(IPythonHandler):
 
     def post(self):
-        statusMsgComm = Comm(target_name='', data='started')
+        statusMsg = {}
+        statusMsg["finished"] = False;
+        statusMsg["text"] = "starting..."
+        statusMsg["ok"] = True;
+        self.write(statusMsg)
+        self.flush()
 
-        curUser = self.get_current_user()
-        userName = curUser['name']
-        
-        githubToken = os.environ['GITHUB_TOKEN']
-        githubUserName = os.environ['GITHUB_USER']
-        githubAcc = Github(githubToken)
-        user = githubAcc.get_user()
-        repos = [r.name for r in githubAcc.get_user().get_repos()]
-        if not 'plutons' in repos:
-            repo = user.create_repo("plutons", "notebooks created on pluto")
-        else:
-            repo = user.get_repo("plutons")
-        
-        jbody = json.loads(self.request.body.decode('utf-8'))
-        #fullPath = os.path.join(os.getcwd(), jbody['notebook'])
-        fullPath = jbody['notebook']
-
-        existingFile = None
         try:
-            existingFile = repo.get_contents(fullPath)
-        except:
-            pass
+            curUser = self.get_current_user()
+            userName = curUser['name']
+        
+            githubToken = os.environ['GITHUB_TOKEN']
+            githubUserName = os.environ['GITHUB_USER']
+            githubAcc = Github(githubToken)
+            user = githubAcc.get_user()
+            repos = [r.name for r in githubAcc.get_user().get_repos()]
+            if not 'plutons' in repos:
+                statusMsg["text"] = "creating repo..."
+                self.write(statusMsg)
+                self.flush()
+                repo = user.create_repo("plutons", "notebooks created on pluto")
+            else:
+                repo = user.get_repo("plutons")
+        
+            jbody = json.loads(self.request.body.decode('utf-8'))
+            #fullPath = os.path.join(os.getcwd(), jbody['notebook'])
+            fullPath = jbody['notebook']
 
-        cmdLine = f"jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace {fullPath}"
-        cpi = subprocess.run(shlex.split(cmdLine), env=os.environ, errors=True)
-        with open(fullPath, mode='rb') as file:
-            fileContent = file.read()
+            existingFile = None
+            try:
+                existingFile = repo.get_contents(fullPath)
+            except:
+                pass
+
+            cmdLine = f"jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace {fullPath}"
+            cpi = subprocess.run(shlex.split(cmdLine), env=os.environ, errors=True)
+            with open(fullPath, mode='rb') as file:
+                fileContent = file.read()
         
-        if existingFile == None:
-            repo.create_file(f"{fullPath}", "request", fileContent)
-        else:
-            print(existingFile.sha)
-            repo.update_file(f"{fullPath}", "request", fileContent, existingFile.sha)
+            if existingFile == None:
+                repo.create_file(f"{fullPath}", "request", fileContent)
+            else:
+                print(existingFile.sha)
+                repo.update_file(f"{fullPath}", "request", fileContent, existingFile.sha)
         
-        mongoPass = urllib.parse.quote_plus(os.environ['MONGO_PASS'])
-        client = pymongo.MongoClient(f"mongodb+srv://explorer:{mongoPass}@cluster0-eyzcm.mongodb.net/test?retryWrites=true&w=majority")
-        db = client.plutoQ
+            mongoPass = urllib.parse.quote_plus(os.environ['MONGO_PASS'])
+            client = pymongo.MongoClient(f"mongodb+srv://explorer:{mongoPass}@cluster0-eyzcm.mongodb.net/test?retryWrites=true&w=majority")
+            db = client.plutoQ
         
-        reqId = db.q.insert_one({ 'file': f"{fullPath}", 
+            reqId = db.q.insert_one({ 'file': f"{fullPath}", 
                                   'createdOn': datetime.now(), 
                                   'isProcessed': False, 
                                   'notebook': Binary(gzip.compress(fileContent)), 
@@ -69,25 +77,36 @@ class RequestHandler(IPythonHandler):
                                   'githubUser': githubUserName,
 				  'requestHost': platform.node()
                                 })
-        qId = ObjectId(reqId.inserted_id)
+            qId = ObjectId(reqId.inserted_id)
 
-        while True:
-            areWeThereYet = db.q.find_one({'_id': qId, 'isProcessed': True})
-            if areWeThereYet != None:
-                break
-            else:
-                sleep(1)
-                print('>', end='', flush=True)
+            statusMsg["text"] = "processing..."
+            self.write(statusMsg)
+            self.flush()
 
-        os.remove(fullPath)
-        fileContent = areWeThereYet['notebook']
-        with open(fullPath, 'wb') as file:
-            file.write(gzip.decompress(fileContent))
+            while True:
+                areWeThereYet = db.q.find_one({'_id': qId, 'isProcessed': True})
+                if areWeThereYet != None:
+                    break
+                else:
+                    sleep(1)
+                    print('>', end='', flush=True)
 
-        #don't delete the request, you may need it for preventing abuse
-        #db.q.delete_one({'_id': qId})
+            os.remove(fullPath)
+            fileContent = areWeThereYet['notebook']
+            with open(fullPath, 'wb') as file:
+                file.write(gzip.decompress(fileContent))
 
-        self.finish({'resp': 'success'})
+            #don't delete the request, you may need it for preventing abuse
+            #db.q.delete_one({'_id': qId})
+
+            statusMsg["text"] = "done!"
+            statusMsg["finished"] = True;
+            self.finish(statusMsg)
+        except Exception as exp:
+            statusMsg["text"] = str(exp)
+            statusMsg["ok"] = False
+            self.finish(statusMsg)
+
 
 def _jupyter_server_extension_paths():
     return [{
